7589b30047b863d1f72ff4352aab1cb0ff044884
Sandesh Sarade <Saradesandesh@gmail.com>
2024-05-24 12:56:24 +0530

    Added arraylist2 code

---
+++ ArrayList/src/com/list/ArrayListDemo2.java
@@ -0,0 +1,17 @@
+package com.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo2 {
+	public static void main(String[] args) {
+		List<Integer> num1 = new ArrayList<Integer>();
+		num1.add(63);
+		num1.add(65);
+		num1.add(82);
+		num1.add(34);
+		num1.add(75);
+		num1.add(15);
+		System.out.println(num1);
+	}
+}
